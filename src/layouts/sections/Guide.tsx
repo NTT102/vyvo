af3974148636b061fe4bc2c6dae42f12b8924154
NTT102 <53190789+NTT102@users.noreply.github.com>
2023-12-27 04:28:16 +0700

    Update Guide.tsx

--- src/layouts/sections/Guide.tsx
+++ src/layouts/sections/Guide.tsx
@@ -94,12 +94,13 @@ const Guide = () => (
                                 </>
                             )}
                         </Disclosure>
+                       
                         <Disclosure>
                             {({ open }) => (
                                 <>
                                     <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white text-black px-4 py-2 text-left text-sm font-medium hover:bg-blue-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
                                         <span className="block text-base first-letter:uppercase font-semibold">
-                                            hướng dẫn mua gói basic 271$
+                                            hướng dẫn mua data NFT Stake
                                         </span>
                                         <IoIosArrowDown
                                             className={`${
@@ -112,33 +113,7 @@ const Guide = () => (
                                     <Disclosure.Panel className="text-sm text-gray-500">
                                         <iframe
                                             className="w-full aspect-video"
-                                            src="https://www.youtube.com/embed/e-HGF2W8YPk?si=2WJP3wdKT2MLtI7O"
-                                            title="YouTube video player"
-                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                        ></iframe>
-                                    </Disclosure.Panel>
-                                </>
-                            )}
-                        </Disclosure>
-                        <Disclosure>
-                            {({ open }) => (
-                                <>
-                                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white text-black px-4 py-2 text-left text-sm font-medium hover:bg-blue-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
-                                        <span className="block text-base first-letter:uppercase font-semibold">
-                                            hướng dẫn mua gói poolkit 1250$
-                                        </span>
-                                        <IoIosArrowDown
-                                            className={`${
-                                                open
-                                                    ? "rotate-180 transform"
-                                                    : ""
-                                            } h-5 w-5 text-black`}
-                                        />
-                                    </Disclosure.Button>
-                                    <Disclosure.Panel className="text-sm text-gray-500">
-                                        <iframe
-                                            className="w-full aspect-video"
-                                            src="https://www.youtube.com/embed/2DY5ZLt7qA8?si=bzA3DVHdGV643gF9"
+                                            src="https://www.youtube.com/embed/POohFM0kpww?si=DNcxYq_twydKx1_I"
                                             title="YouTube video player"
                                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         ></iframe>
@@ -165,7 +140,7 @@ const Guide = () => (
                                     <Disclosure.Panel className="text-sm text-gray-500">
                                         <iframe
                                             className="w-full aspect-video"
-                                            src="https://youtube.com/embed/p5MHK3rl6vc?si=rCnNsoXAWoSa8DlB"
+                                            src="https://www.youtube.com/embed/5WYMfxNf4E4?si=1ekBlICLMibpeqy-"
                                             title="YouTube video player"
                                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         ></iframe>
