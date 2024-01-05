b803a561073d588430d816eda251942dcd79894e
NTT102 <53190789+NTT102@users.noreply.github.com>
2024-01-06 03:43:11 +0700

    Update Guide.tsx

--- src/layouts/sections/Guide.tsx
+++ src/layouts/sections/Guide.tsx
@@ -584,6 +584,32 @@ const Guide = () => (
                                 <>
                                     <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white text-black px-4 py-2 text-left text-sm font-medium hover:bg-blue-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
                                         <span className="block text-base first-letter:uppercase font-semibold">
+                                            Opp về dự án Inpersona (30/12/2023) - Cofounder VGroup JustinLe
+                                        </span>
+                                        <IoIosArrowDown
+                                            className={`${
+                                                open
+                                                    ? "rotate-180 transform"
+                                                    : ""
+                                            } h-5 w-5 text-black`}
+                                        />
+                                    </Disclosure.Button>
+                                    <Disclosure.Panel className="text-sm text-gray-500">
+                                        <iframe
+                                            className="w-full aspect-video"
+                                            src="https://www.youtube.com/embed/al7duH-REDo?si=ZI-vg77Xn-3IYquH"
+                                            title="YouTube video player"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                        ></iframe>
+                                    </Disclosure.Panel>
+                                </>
+                            )}
+                        </Disclosure>
+                        <Disclosure>
+                            {({ open }) => (
+                                <>
+                                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white text-black px-4 py-2 text-left text-sm font-medium hover:bg-blue-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
+                                        <span className="block text-base first-letter:uppercase font-semibold">
                                             Đào tạo chuyên sâu về dự án
                                             Inpersona - Cofounder VGroup JustinLe
                                         </span>
