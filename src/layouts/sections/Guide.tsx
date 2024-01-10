f268127c06e32b4ca3b24034084da0c953643fc3
NTT102 <53190789+NTT102@users.noreply.github.com>
2024-01-10 12:12:37 +0700

    Update Guide.tsx

--- src/layouts/sections/Guide.tsx
+++ src/layouts/sections/Guide.tsx
@@ -610,8 +610,7 @@ const Guide = () => (
                                 <>
                                     <Disclosure.Button className="flex w-full justify-between rounded-lg bg-white text-black px-4 py-2 text-left text-sm font-medium hover:bg-blue-100 focus:outline-none focus-visible:ring focus-visible:ring-blue-500/75">
                                         <span className="block text-base first-letter:uppercase font-semibold">
-                                            Đào tạo chuyên sâu về dự án
-                                            Inpersona - Cofounder VGroup JustinLe
+                                            Đào tạo chuyên sâu về dự án Inpersona (09/01/2024) - Cofounder VGroup JustinLe
                                         </span>
                                         <IoIosArrowDown
                                             className={`${
@@ -624,7 +623,7 @@ const Guide = () => (
                                     <Disclosure.Panel className="text-sm text-gray-500">
                                         <iframe
                                             className="w-full aspect-video"
-                                            src="https://www.youtube.com/embed/al7duH-REDo?si=ZI-vg77Xn-3IYquH"
+                                            src="https://www.youtube.com/embed/7tlEGRhHdZ4?si=kVbLCIlxLXv-OFm9"
                                             title="YouTube video player"
                                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         ></iframe>
