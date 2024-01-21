f66aed5a9428b108c7d2bca6ea5d0c210e412b57
NTT102 <53190789+NTT102@users.noreply.github.com>
2024-01-22 02:24:29 +0700

    Update index.ts

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,4 +1,3 @@
-export { default as Hero } from "./sections/Hero";
 export { default as Guide } from "./sections/Guide";
 export { default as Product } from "./sections/Product";
 export { default as Footer } from "./Footer";
