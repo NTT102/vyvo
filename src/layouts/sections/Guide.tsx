ae92f80641e7117d4333056acd9d76edea58ca52
NTT102 <53190789+NTT102@users.noreply.github.com>
2024-01-22 02:21:48 +0700

    Update Guide.tsx

--- src/layouts/sections/Guide.tsx
+++ src/layouts/sections/Guide.tsx
@@ -140,7 +140,7 @@ const Guide = () => (
                                     <Disclosure.Panel className="text-sm text-gray-500">
                                         <iframe
                                             className="w-full aspect-video"
-                                            src="https://www.youtube.com/embed/5WYMfxNf4E4?si=1ekBlICLMibpeqy-"
+                                            src="https://youtube.com/shorts/JZBu1mUD4EU?si=JEzZOGm2hRtijVJA"
                                             title="YouTube video player"
                                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         ></iframe>
